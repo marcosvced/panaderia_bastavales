971848f7858e2eb459e5e8d9c09145871a929780
Marcos <devler.mve@gmail.com>
2024-07-03 11:17:47 +0200

    feat: opening hours has been added to desktop mode

--- src/assets/styles/unocss/_settings/palette.ts
+++ src/assets/styles/unocss/_settings/palette.ts
@@ -11,8 +11,17 @@ const primary = {
     primary: 'hsla(33, 42%, 60%, 1)'
 }
 
+const opacity = {
+    'opacity-100': 'hsla(60, 100%, 95%, 0.1)',
+    'opacity-200': 'hsla(60, 100%, 95%, 0.2)',
+    'opacity-300': 'hsla(60, 100%, 95%, 0.3)',
+    'opacity-400': 'hsla(60, 100%, 95%, 0.4)',
+    'opacity-500': 'hsla(60, 100%, 95%, 0.5)',
+}
+
 export const colors = {
     ...ash,
     ...light,
-    ...primary
+    ...primary,
+    ...opacity
 }
