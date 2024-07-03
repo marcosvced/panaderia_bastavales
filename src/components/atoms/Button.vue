967b49db4c473c8186534554b8c6724f10cc0d74
Marcos <devler.mve@gmail.com>
2024-07-03 13:21:11 +0200

    feat: several improvements has been added

---
+++ src/components/atoms/Button.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+interface Props {
+  text: string
+  href?: string
+  onClick?: void
+}
+
+const props = withDefaults(defineProps<Props>(), {})
+
+const onClick = () => {
+  if (props.onClick) {
+    props.onClick()
+    return
+  }
+  if(props.href) {
+
+    window['lenis'].scrollTo(props.href)
+    window.location.href = `${window.location.origin}${props.href}`
+    return
+  }
+}
+</script>
+
+<template>
+  <button type="button" :text="text" @click="onClick">
+    {{ text }}
+  </button>
+</template>
+
+<style scoped>
+button {
+  background: none;
+  border: none;
+  font-family: Inter;
+  font-weight: 200;
+  text-transform: uppercase;
+  position: relative;
+  padding-inline: 32px;
+  padding-block: 16px;
+  cursor: pointer;
+  color: theme('colors.light');
+}
+
+button:before {
+  content: "";
+  border: 1px solid white;
+  height: 46px;
+  width: 46px;
+  border-radius: 23px;
+  position: absolute;
+  left: 0;
+  top: 50%;
+  transform: translateY(-50%);
+  background-color: rgba(255, 255, 255, 0);
+  transition: all .35s ease-in-out;
+  opacity: .25;
+}
+
+button:hover:before {
+  background-color: rgba(255, 255, 255, .25);
+  width: 100%;
+}
+</style>
